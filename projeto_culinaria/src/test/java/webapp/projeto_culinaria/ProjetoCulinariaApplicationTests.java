68ba86cbf1741a2d533d3c6cd8a6e303458641ea
Alberto-MeloL <albertomeloteco@gmail.com>
2024-05-06 13:48:50 -0300

    Criação de Projeto

---
+++ projeto_culinaria/src/test/java/webapp/projeto_culinaria/ProjetoCulinariaApplicationTests.java
@@ -0,0 +1,13 @@
+package webapp.projeto_culinaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCulinariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
